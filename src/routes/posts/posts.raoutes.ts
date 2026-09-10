7132d802b8fd48f9cce46df7c1320b4cb1ba532d
Ghina <sistyaavinza@gmail.com>
2026-09-11 06:55:30 +0700

    feat: membuat feature create

--- src/routes/posts/posts.raoutes.ts
+++ src/routes/posts/posts.raoutes.ts
@@ -6,19 +6,23 @@ import { authenticate } from "../../middleware/auth.middleware";
 const router = Router();
 
 // CREATE
-router.post('/' , authenticate, uploadSingleImage , postsController.createPosts);
+router.post("/", authenticate, uploadSingleImage, postsController.createPosts);
 
 // GET POST ALL
-router.get('/' , postsController.getPosts);
+router.get("/", postsController.getPosts);
 
 // GET POST BY ID
-router.get('/:id' , postsController.getPostById);
+router.get("/:id", postsController.getPostById);
 
 // UPDATE
-router.patch('/:id' , authenticate, uploadSingleImage, postsController.updatePost);
+router.patch(
+  "/:id",
+  authenticate,
+  uploadSingleImage,
+  postsController.updatePost,
+);
 
 // DELTE
-router.delete('/:id' , authenticate, postsController.deletePost);
-
+router.delete("/:id", authenticate, postsController.deletePost);
 
 export default router;
