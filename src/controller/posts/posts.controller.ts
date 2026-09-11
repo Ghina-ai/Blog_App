72e49dbf4ce20c7ffc3df9b1d5b9b5b4d280059f
Ghina <sistyaavinza@gmail.com>
2026-09-11 08:17:41 +0700

    feat: menambah fitur delete

--- src/controller/posts/posts.controller.ts
+++ src/controller/posts/posts.controller.ts
@@ -14,52 +14,41 @@ import {
 } from "../../servies/cloudinary.service";
 
 export class PostsController {
-  // CREATE
-  createPosts = async (req: Request, res: Response) => {
-    try {
-      const validateData = createPostSchema.parse(req.body);
-      const { userId, title, content, categoryId } = validateData;
-
-      let imageUrl: string | undefined;
-      let imagePublicId: string | undefined;
-
-      if (req.file) {
-        const uploadResult = await uploadToCloudinary(req.file.buffer);
-        imageUrl = uploadResult.secure_url;
-        imagePublicId = uploadResult.public_id;
+  //DELETE
+    deletePost = async (req: Request, res: Response) => {
+      try {
+        // 1. VALIDATE POST ID
+        const validateParams = postIdSchema.parse(req.params);
+        const { id } = validateParams;
+  
+        // 2.CEK POST
+        const existingPost = await db.query.postsTable.findFirst({
+          where: eq(postsTable.id, id),
+        });
+  
+        if (!existingPost) {
+          return res.status(404).json({
+            success: false,
+            message: "post not found",
+          });
+        }
+  
+        // 3. DELETE PERMANEN DARI DATABASE
+        await db.delete(postsTable).where(eq(postsTable.id, id));
+  
+        // 4.RESPONSE
+        return res.status(200).json({
+          success: true,
+          message: "post deleted successfully",
+        });
+      } catch (error: any) {
+        console.error("delete post error:,", error);
+        return res.status(500).json({
+          success: false,
+          message: "internal server error",
+          error: error.message,
+        });
       }
-
-      const [insertedPost] = await db
-        .insert(postsTable)
-        .values({
-          userId,
-          title,
-          content,
-          categoryId,
-          imageUrl,
-          imagePublicId,
-        })
-        .$returningId();
-
-      const newPost = await db.query.postsTable.findFirst({
-        where: eq(postsTable.id, insertedPost.id),
-      });
-
-      return res.status(201).json({
-        success: true,
-        message: "post created successfully",
-        data: {
-          post: newPost,
-        },
-      });
-    } catch (error) {
-      console.error("create post error:", error);
-      return res.status(500).json({
-        success: false,
-        message: "terjadi kesalahan pada server",
-        error: error instanceof Error ? error.message : error,
-      });
-    }
-  };
+    };
 
 }
