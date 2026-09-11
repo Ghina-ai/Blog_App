b82247d8c77abd22e8577f97d2f61a52b7d91d77
Ghina <sistyaavinza@gmail.com>
2026-09-11 08:16:15 +0700

    feat: menambah fitur update

--- src/controller/posts/posts.controller.ts
+++ src/controller/posts/posts.controller.ts
@@ -14,52 +14,87 @@ import {
 } from "../../servies/cloudinary.service";
 
 export class PostsController {
-  // CREATE
-  createPosts = async (req: Request, res: Response) => {
-    try {
-      const validateData = createPostSchema.parse(req.body);
-      const { userId, title, content, categoryId } = validateData;
-
-      let imageUrl: string | undefined;
-      let imagePublicId: string | undefined;
-
-      if (req.file) {
-        const uploadResult = await uploadToCloudinary(req.file.buffer);
-        imageUrl = uploadResult.secure_url;
-        imagePublicId = uploadResult.public_id;
+  //UPDATE
+    updatePost = async (req: Request, res: Response) => {
+      try {
+        // 1. VALIDASI PARAMS
+        const validateParams = updatePostParamsSchema.parse(req.params);
+        const { id } = validateParams;
+  
+        // 2. VALIDATE BODY
+        const validateData = updatePostSchema.parse(req.body);
+        const { title, content } = validateData;
+  
+        // 3. CEK POST
+        const [existingPost] = await db
+          .select()
+          .from(postsTable)
+          .where(eq(postsTable.id, id));
+  
+        if (!existingPost) {
+          return res.status(404).json({
+            success: false,
+            message: "post not found",
+          });
+        }
+  
+        // 4. SIAPKAN DATA UPDATE
+        let imageUrl = existingPost.imageUrl;
+        let imagePublicId = existingPost.imagePublicId;
+  
+        // 5.JIKA ADA IMAGE BARU
+        if (req.file) {
+          const uploadResult = await uploadToCloudinary(req.file.buffer);
+          imageUrl = uploadResult.secure_url;
+          imagePublicId = uploadResult.public_id;
+  
+          // HAPUS IMAGE LAMA
+          if (existingPost.imagePublicId) {
+            await deleteFromCloudinary(existingPost.imagePublicId);
+          }
+        }
+  
+        // 6.UPDATE DATABASE
+        await db
+          .update(postsTable)
+          .set({
+            ...(title !== undefined && {
+              title,
+            }),
+  
+            ...(content !== undefined && {
+              content,
+            }),
+  
+            ...(req.file && {
+              imageUrl,
+              imagePublicId,
+            }),
+          })
+          .where(eq(postsTable.id, id));
+  
+        // 7.AMBIL DATA TERBARU
+        const [updatePost] = await db
+          .select()
+          .from(postsTable)
+          .where(eq(postsTable.id, id));
+  
+        // 8. RESPONSE
+        return res.status(200).json({
+          success: true,
+          message: "post updates successfully",
+          data: {
+            post: updatePost,
+          },
+        });
+      } catch (error: any) {
+        console.error("ipdate post error:", error);
+        return res.status(500).json({
+          success: false,
+          message: "internal server error",
+          error: error.message,
+        });
       }
-
-      const [insertedPost] = await db
-        .insert(postsTable)
-        .values({
-          userId,
-          title,
-          content,
-          categoryId,
-          imageUrl,
-          imagePublicId,
-        })
-        .$returningId();
-
-      const newPost = await db.query.postsTable.findFirst({
-        where: eq(postsTable.id, insertedPost.id),
-      });
-
-      return res.status(201).json({
-        success: true,
-        message: "post created successfully",
-        data: {
-          post: newPost,
-        },
-      });
-    } catch (error) {
-      console.error("create post error:", error);
-      return res.status(500).json({
-        success: false,
-        message: "terjadi kesalahan pada server",
-        error: error instanceof Error ? error.message : error,
-      });
-    }
-  };
+    };
 
 }
