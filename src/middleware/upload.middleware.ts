63eac144fb404eaddda2888e38cc6d804442fcb4
Ghina <sistyaavinza@gmail.com>
2026-09-15 01:22:48 +0700

    feat: perubahan middleware

--- src/middleware/upload.middleware.ts
+++ src/middleware/upload.middleware.ts
@@ -5,15 +5,39 @@ const storage = multer.memoryStorage();
 const upload = multer({
     storage,
     limits: {
-        fileSize: 5 * 1024 * 1024, //max 5MB
+        fileSize: 5 * 1024 * 1024, // max 5MB
     },
     fileFilter: (_req, file, cb) => {
-        if (file.mimetype.startsWith("image/")) {
+        const allowedMimeTypes = [
+            "image/jpeg",
+            "image/jpg",
+            "image/png",
+            "image/webp",
+            "image/gif",
+        ];
+
+        const allowedExtensions = [
+            ".jpg",
+            ".jpeg",
+            ".png",
+            ".webp",
+            ".gif",
+        ];
+
+        const fileExtension = file.originalname
+            .toLowerCase()
+            .substring(file.originalname.lastIndexOf("."));
+
+        if (
+            allowedMimeTypes.includes(file.mimetype) ||
+            allowedExtensions.includes(fileExtension)
+        ) {
             cb(null, true);
         } else {
             cb(new Error("hanya file gambar yang diperbolehkan!!"));
         }
     },
-});//"image" adalah nama key/field saat upload file
+});
 
+// "image" adalah nama key/field saat upload file
 export const uploadSingleImage = upload.single("image");
