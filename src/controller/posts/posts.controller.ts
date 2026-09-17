069e987a56e2cd2ea30d76f2dcc244bb8582b099
Ghina <sistyaavinza@gmail.com>
2026-09-17 22:16:13 +0700

    feat: update create

--- src/controller/posts/posts.controller.ts
+++ src/controller/posts/posts.controller.ts
@@ -7,56 +7,54 @@ import { deleteFromCloudinary, uploadToCloudinary } from "../../servies/cloudina
 
 
 export class PostsController {
-    // CREATE
-    createPosts  = async(req: Request, res: Response) => {
-        try {
-            const validateData = createPostSchema.parse(req.body);
-            const { userId, title, content, categoryId } = validateData;
-
-            let imageUrl: string | undefined;
-            let imagePublicId: string | undefined;
-
-            if (req.file) {
-                const uploadResult = await uploadToCloudinary(req.file.buffer);
-                imageUrl = uploadResult.secure_url;
-                imagePublicId = uploadResult.public_id;
-            }
-
-            const [insertedPost] = await db
-                .insert(postsTable)
-                .values({ 
-                    userId, 
-                    title, 
-                    content,
-                    categoryId, 
-                    imageUrl, 
-                    imagePublicId, 
-            })
-            .$returningId();
-
-            const newPost = await db.query.postsTable.findFirst({ where: eq(postsTable.id, insertedPost.id) });
-
-            return res.status(201).json({
-                success: true,
-                message: "post created successfully",
-                data: {
-                    post: newPost,
-                },
-            });
-
-        } catch (error) {
-            console.error("create post error:", error);
-            return res.status(500).json({
-                success: false,
-                message: 
-                    "terjadi kesalahan pada server",
-                error: error instanceof Error
-                    ? error.message
-                    : error,
-            });
-
-        }
-    }
+   // CREATE
+    createPosts = async (req: Request, res: Response) => {
+         try {
+         const validateData = createPostSchema.parse(req.body);
+         const { title, content, categoryId } = validateData;
+         const userId = 4;
+         
+         let imageUrl: string | undefined;
+         let imagePublicId: string | undefined;
+   
+         if (req.file) {
+           const uploadResult = await uploadToCloudinary(req.file.buffer);
+           imageUrl = uploadResult.secure_url;
+           imagePublicId = uploadResult.public_id;
+         }
+   
+         const [insertedPost] = await db
+           .insert(postsTable)
+           .values({
+             userId,
+             title,
+             content,
+             categoryId,
+             imageUrl,
+             imagePublicId,
+           })
+           .$returningId();
+   
+         const newPost = await db.query.postsTable.findFirst({
+           where: eq(postsTable.id, insertedPost.id),
+         });
+   
+         return res.status(201).json({
+           success: true,
+           message: "post created successfully",
+           data: {
+           post: newPost,
+           },
+         });
+         } catch (error) {
+         console.error("create post error:", error);
+         return res.status(500).json({
+           success: false,
+           message: "terjadi kesalahan pada server",
+           error: error instanceof Error ? error.message : error,
+         });
+         }
+    };
 
 }
 
