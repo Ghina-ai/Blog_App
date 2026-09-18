9f5338d5ceab77c2bfb4949f1b4c43e884eafe53
Ghina <sistyaavinza@gmail.com>
2026-09-18 11:35:20 +0700

    feat: fitur auth

--- src/routes/category/categorys.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import categoriesController from "../../controller/categorys/category.controller";
-
-const router = Router();
-
-router.post("/", categoriesController.createCategory);
-
-export default router;
